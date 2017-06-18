69202a23b697fd84b65f46c1f84be206b9fcdf01
Stu Peters <stu_peters187@hotmail.com>
2017-06-18 11:11:05 -0400

    coin contract complete

--- SimpleCoin/SimpleCoin.sol
+++ SimpleCoin/SimpleCoin.sol
@@ -1,7 +1,8 @@
 pragma solidity 0.4.11;
 
+
 contract Coin {
-    
+
     address owner;
     uint public totalSupply;
     
@@ -11,35 +12,39 @@ contract Coin {
     event NewCoinLog(address _to, uint _amount, uint _newSupply);
     
     modifier onlyOwner(){
-        if(msg.sender != owner) {
-            throw;
-        } else {
+      if(msg.sender != owner) {
+        throw;
+      } else {
             _;
-        }
+      }
     }
-    
+
     function Coin(uint _supply) {
       owner = msg.sender;
       totalSupply = _supply;
       balances[owner] += _supply;
     }
-    
+
     function getBalance(address _addr) constant returns (uint){
-        return balances[_addr];
+      return balances[_addr];
     }
-    
+
     function transfer(address _to, uint _amount) returns (bool) {
-        if(balances[msg.sender] < _amount) throw;
-        balances[msg.sender] -= _amount;
-        balances[_to] += _amount;
-        Transfer(_to, msg.sender, _amount);
-        return true;
+      if(balances[msg.sender] < _amount) throw;
+      balances[msg.sender] -= _amount;
+      balances[_to] += _amount;
+      Transfer(_to, msg.sender, _amount);
+      return true;
     }
-    
+
     function mint(uint _amount) onlyOwner returns (bool){
-        totalSupply += _amount;
-        balances[owner] += _amount;
-        NewCoinLog(owner, _amount, totalSupply);
-        return true;
+      totalSupply += _amount;
+      balances[owner] += _amount;
+      NewCoinLog(owner, _amount, totalSupply);
+      return true;
+    }
+
+    function disable() onlyOwner {
+        selfdestruct(owner);
     }
 }
